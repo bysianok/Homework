2a727bd6b4d4714f98692ad0a742022fd718ef3c
bysianok <129299247+bysianok@users.noreply.github.com>
2023-06-06 12:20:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let header = document.querySelector('header');
+let li = document.querySelectorAll('li');
+let btn = document.querySelector('button');
+let video = document.querySelector('video');
+let audio = document.querySelector('audio');
+li.forEach(function(item, index){
+    item.addEventListener('click', () => {
+        console.log(item, index);
+        li.forEach(function(item) {
+            item.classList.remove('active');
+        });
+        item.classList.add('active');
+        btn.classList.remove('active');
+        video.src = `video/${index}.mp4`;
+        audio.src = `audio/${index}.mp3`;
+        video.style.opacity = '1';
+    });
+});
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+    if (btn.classList.contains('active')) {
+        audio.play();
+        video.play();
+        header.style.opacity = '0';
+        btn.style.opacity = '0.1';
+        video.classList.add('active');
+        console.log(video.duration);
+        video.style.animationDuration = `${video.duration}s`;
+    }
+    else {
+        audio.pause();
+        video.pause();
+        video.currentTime = 0;
+        header.style.opacity = '1';
+        btn.style.opacity = '1';
+        video.style.opacity = '0.5';
+        video.classList.remove('active');
+    }
+});
